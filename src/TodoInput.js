c519508615182da46cbc9523455bbe1b8b119dd5
xiaojiongtu <18377335170@163.com>
2017-10-12 16:47:42 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,7 +4,11 @@
 import React, { Component } from 'react';
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" defaultValue={this.props.content}/>
+        return <input type="text" defaultValue={this.props.content} onkeypress={this.submit}/>
     }
-
+     submit(e){
+         if(e.key==="Enter"){
+            console.log("用户按回车了")
+         }
+     }
 }
