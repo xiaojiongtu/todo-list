2bc375d4a3381bb4cd0014a2b626405ba069eb0e
xiaojiongtu <18377335170@163.com>
2017-10-13 11:45:55 +0800

    删除todo

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ class App extends Component {
       let todos=this.state.todoList.map((item,index)=>{
           return(
               <li key={index}>
-                  <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
+                  <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                                           onDelete={this.delete.bind(this)}
+                  />
               </li>
           )
       });
@@ -61,7 +63,10 @@ class App extends Component {
             todoList:this.state.todoList
         })
     }
-
+    delete(event,todo){
+        todo.deleted=true
+        this.setState(this.state)
+    }
 }
 
 export default App;
